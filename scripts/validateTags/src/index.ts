b78a3d04b624dfdec796e4c4e6b33863e63388b1
xHyroM <generalkubo@gmail.com>
2022-07-16 14:32:58 +0200

    chore(scripts/validateTags): fix import

--- scripts/validateTags/src/index.ts
+++ scripts/validateTags/src/index.ts
@@ -2,7 +2,7 @@ const files = await Bun.file('./files.json').text();
 if (!files.includes('files/tags.toml')) process.exit(0);
 
 // @ts-expect-error types
-const tags = await import('./files/tags.toml');
+const tags = await import('../../../files/tags.toml');
 console.log(tags);
 
 export { };
