fa4a844dcb2e73d0485297aeb7b69ae2791c2996
xHyroM <xhyrom.hyro@gmail.com>
2023-08-25 07:55:31 +0200

    feat: nickcname moderation

--- src/listeners/index.ts
+++ src/listeners/index.ts
@@ -1,3 +1,4 @@
 import "./ready.ts";
 import "./interaction_create.ts";
 import "./message_create.ts";
+import "./nickname_moderation.ts";
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,9 @@
 export function safeSlice<T>(array: T[], length: number) {
   return array.length > length ? array.slice(0, length) : array;
 }
+
+export async function silently<T>(value: Promise<T>) {
+  try {
+    await value;
+  } catch {}
+}
