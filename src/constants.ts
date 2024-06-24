0026dedd977934b5bb9670a0a2661b6dbaa29860
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-06-24 20:16:52 +0200

    feat: add bunsob

--- src/constants.ts
+++ src/constants.ts
@@ -44,6 +44,7 @@ export const BUN_EMOJIS = [
   { name: "octobun", id: "1210663393480941598" },
   { name: "bunparty", id: "1210663548963921960" },
   { name: "bunsplode", id: "1210663666454634586" },
+  { name: "bunsob", id: "1254862523857895537" },
   { name: "bundowspet", id: "1222860633377341461", animated: true },
   { name: "bunpet", id: "1172808445737574591", animated: true },
   { name: "bunsegfault", id: "1175208306533486632", animated: true },
