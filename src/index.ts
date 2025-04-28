86bf78cf16ec8c94c61d3c4f8f815dbe81d8c8d6
Jozef Steinhübl <generalkubo@gmail.com>
2025-04-28 09:07:00 +0000

    yey

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import "./loaders/tags.ts";
 
 import { defaultTransformers, handler } from "./handler.ts";
-import { Intents, createClient } from "lilybird";
+import { Client, Intents, createClient } from "lilybird";
 import { info } from "@paperdave/logger";
+import { MergeTransformers } from "@lilybird/transformers";
 
 // Make sure bubu will not crash
 process.on("unhandledRejection", console.error);
@@ -13,7 +14,7 @@ handler.cachePath = `${import.meta.dir}/lily-cache/handler`;
 await handler.scanDir(`${import.meta.dir}/commands`);
 await handler.scanDir(`${import.meta.dir}/listeners`);
 
-await createClient({
+await createClient<MergeTransformers<Client, typeof defaultTransformers>>({
   token: process.env.DISCORD_BOT_TOKEN,
   intents: Intents.GUILDS | Intents.GUILD_MESSAGES | Intents.MESSAGE_CONTENT | Intents.GUILD_MEMBERS,
   listeners: {
@@ -21,7 +22,7 @@ await createClient({
       info(`Logged in as ${client.user.username} (${client.user.id})`);
       await handler.loadGlobalCommands(client);
     },
+    ...handler.getListenersObject(),
   },
-  ...handler.getListenersObject(),
-  ...defaultTransformers,
+  transformers: defaultTransformers,
 });
